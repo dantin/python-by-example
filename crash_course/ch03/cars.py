e5a07f48b9a2f412a0ca23cb1c722b94ef961f27
dantin <chengjie.ding@gmail.com>
2018-08-10 14:38:25 +0800

    demo that organize a list.

---
+++ crash_course/ch03/cars.py
@@ -0,0 +1,16 @@
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# NOTE: sort() method permanently change list content.
+# cars.sort(reverse=True)
+# cars.sort()
+
+# NOTE: sorted() function change list temporarily.
+print('Here is the original list:')
+print(cars)
+
+print('\nHere is the sorted list:')
+print(sorted(cars))
+
+print('\nHere is the original list again:')
+print(cars)
