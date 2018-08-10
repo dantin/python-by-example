8268f807235c15c5e11b3c4ccccfece0f656c416
dantin <chengjie.ding@gmail.com>
2018-08-10 14:51:39 +0800

    exec.

---
+++ crash_course/ch03/exec/dinner_guests.py
@@ -0,0 +1,4 @@
+
+guests = ['weiying', 'zhenhua', 'jiayi']
+print(guests)
+print(len(guests), 'guests.')
---
+++ crash_course/ch03/exec/every_function.py
@@ -0,0 +1,4 @@
+
+languages = ['c++', 'c', 'python', 'java', 'golang']
+for lang in languages:
+    print('learned', lang)
