637d7fefc00de417929678a9e45ded73b0f93de3
dantin <chengjie.ding@gmail.com>
2018-08-10 11:37:09 +0800

    solution of part iii.

---
+++ crash_course/ch02/exec/favorite_number.py
@@ -0,0 +1,4 @@
+
+number = 56
+message = 'My favorite number is ' + str(number)
+print(message)
---
+++ crash_course/ch02/exec/number_eight.py
@@ -0,0 +1,6 @@
+
+print(5 + 3)
+print(9 - 1)
+print(2 * 4)
+# print(24 / 3) # NOTE: default is float result
+print(24 // 3)
