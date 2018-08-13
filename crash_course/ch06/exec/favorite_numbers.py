3c1148106320ac536da06f262192bd6c82be28b8
dantin <chengjie.ding@gmail.com>
2018-08-13 13:51:06 +0800

    exec.

---
+++ crash_course/ch06/exec/favorite_numbers.py
@@ -0,0 +1,10 @@
+
+favorite_numbers = {
+    'david': 43,
+    'jiayi': 17,
+    'zhiyuan': 7,
+    'zhenhua': 3,
+}
+
+for k, v in favorite_numbers.items():
+    print(k + ' likes ' + str(favorite_numbers[k]) + '.')
---
+++ crash_course/ch06/exec/persion.py
@@ -0,0 +1,12 @@
+
+person = {
+    'first_name': 'henry',
+    'last_name': 'han',
+    'age': 36,
+    'city': 'shanghai',
+}
+
+print('first name: ' + person['first_name'])
+print('last name:  ' + person['last_name'])
+print('age:        ' + str(person['age']))
+print('city:       ' + person['city'])
