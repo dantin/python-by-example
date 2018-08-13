03b70646b1ef25d17c7e6ca79ed1a24a7eccc4b9
dantin <chengjie.ding@gmail.com>
2018-08-13 14:09:25 +0800

    loop through keys in dictionary.

--- crash_course/ch06/favorite_languages2.py
+++ crash_course/ch06/favorite_languages2.py
@@ -6,6 +6,8 @@ favorite_languages = {
     'phil': 'python',
 }
 
+# default through key
+# for name, language in favorite_languages:
 for name, language in favorite_languages.items():
     print(name.title() + '\'s favorite language is ' +
           language.title() + '.')
