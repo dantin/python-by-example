81003575441b4d09cc6bd07781220deb947574c7
dantin <chengjie.ding@gmail.com>
2018-08-14 13:26:13 +0800

    exec.

---
+++ crash_course/ch07/exec/multiples_of_ten.py
@@ -0,0 +1,6 @@
+
+number = input('Guess a number: ')
+number = int(number)
+
+if number % 10 == 0:
+    print(str(number) + ' is a multiple of 10.')
---
+++ crash_course/ch07/exec/rental_car.py
@@ -0,0 +1,3 @@
+
+car = input('What kind of rental car you would like? ')
+print('Let me see if I can find you a ' + car.lower().title() + '.')
