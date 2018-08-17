d512f0abfdeab2730498687f2b1468745644ab46
dantin <chengjie.ding@gmail.com>
2018-08-17 16:32:18 +0800

    split key up/down event.

--- crash_course/alien_invasion/game_functions.py
+++ crash_course/alien_invasion/game_functions.py
@@ -2,7 +2,22 @@
 import pygame
 import sys
 
-from pygame.locals import KEYUP, KEYDOWN
+
+def check_keydown_events(event, ship):
+    """Respond to key presses."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 
 def check_events(ship):
     """Respond to key-presses and mouse events."""
@@ -11,16 +26,9 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
