1acff4adb589e9af35d3338a037632b64a7b395f
David Ding <chengjie.ding@gmail.com>
2018-08-20 11:47:20 +0800

    chapter 12 of python crash course. (#15)
    * demo of creating a pygame window and response to user input.
    
    * set background.
    
    * create setting class.
    
    * draw ship on the screen.
    
    * refactoring code.
    
    * support move left and right.
    
    * adjust speed.
    
    * limit ship range.
    
    * split key up/down event.
    
    * format code.
    
    * exec.
    
    * simplify exec code.
    
    * support bullet.
    
    * delete bullet if it moves out of view.
    
    * fix comment.
    
    * exec.
    
    * move exec to chapter 12.

---
+++ crash_course/ch12/exec/rocket.py
@@ -0,0 +1,128 @@
+
+import pygame
+import sys
+
+
+class Settings():
+    """A class to store all settings."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 400
+        self.screen_height = 300
+        self.bg_color = (230, 230, 230)
+
+        self.rocket_speed_factor= 1.5
+
+
+class Rocket():
+    """Rocket class."""
+
+    def __init__(self, settings, screen):
+        """Initialize Rocket using parameters."""
+        self.settings = settings
+        self.screen = screen
+
+        # load the rocket image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new Rocket at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Store a decimal value to the rocket's center.
+        self.center = float(self.rect.centerx)
+        self.bottom = float(self.rect.bottom)
+
+        # Movement flag
+        self.moving_left = False
+        self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update the rocket's position based on the movement flag."""
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.settings.rocket_speed_factor
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.settings.rocket_speed_factor
+        if self.moving_up and self.rect.top > 0:
+            self.bottom -= self.settings.rocket_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.bottom += self.settings.rocket_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
+        self.rect.bottom = self.bottom
+
+    def blitme(self):
+        """Draw the rocket at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+
+def check_keydown_events(event, rocket):
+    """Respond to key presses."""
+    if event.key == pygame.K_LEFT:
+        rocket.moving_left = True
+    elif event.key == pygame.K_RIGHT:
+        rocket.moving_right = True
+    elif event.key == pygame.K_UP:
+        rocket.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        rocket.moving_down = True
+
+
+def check_keyup_events(event, rocket):
+    """Respond to key releases."""
+    if event.key == pygame.K_LEFT:
+        rocket.moving_left = False
+    elif event.key == pygame.K_RIGHT:
+        rocket.moving_right = False
+    elif event.key == pygame.K_UP:
+        rocket.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        rocket.moving_down = False
+
+
+def check_events(rocket):
+    """Respond to key presses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, rocket)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, rocket)
+
+
+def update_screen(settings, screen, rocket):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(settings.bg_color)
+    rocket.blitme()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
+
+
+def run():
+    """Initialize pygame, settings and screen objects."""
+    pygame.init()
+    settings = Settings()
+    screen = pygame.display.set_mode(
+        (settings.screen_width, settings.screen_height))
+    pygame.display.set_caption('Rocket')
+
+    rocket = Rocket(settings, screen)
+
+    # Main Loop
+    while True:
+        check_events(rocket)
+        rocket.update()
+        update_screen(settings, screen, rocket)
+
+
+run()
---
+++ crash_course/ch12/exec/sideways_shooter.py
@@ -0,0 +1,182 @@
+
+import pygame
+import sys
+
+from pygame.sprite import Sprite
+from pygame.sprite import Group
+
+
+class Settings():
+    """A class to store all settings."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 400
+        self.screen_height = 300
+        self.bg_color = (255, 255, 255)
+
+        self.rocket_speed_factor= 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_limit = 3
+
+
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship."""
+
+    def __init__(self, settings, screen, ship):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = screen
+        self.screen_rect = self.screen.get_rect()
+
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, settings.bullet_width, settings.bullet_height)
+        self.rect.centery = ship.rect.centery
+        self.rect.right = ship.rect.right
+
+        # Store the bullet's position as a decimal value.
+        self.x = float(self.rect.x)
+
+        self.color = settings.bullet_color
+        self.speed_factor = settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet right the screen."""
+        # Update the decimal position of the bullet.
+        self.x += self.speed_factor
+        # Update the rect position.
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
+
+
+class Rocket():
+    """Rocket class."""
+
+    def __init__(self, settings, screen):
+        """Initialize Rocket using parameters."""
+        self.settings = settings
+        self.screen = screen
+
+        # load the rocket image and get its rect.
+        self.image = pygame.image.load('images/airplane.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new Rocket at the left center of the screen.
+        self.rect.left = 0
+        self.rect.centery= self.screen_rect.centery
+
+        # Store a decimal value to the rocket's center.
+        self.y = float(self.rect.y)
+
+        # Movement flag
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update the rocket's position based on the movement flag."""
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.rocket_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.rocket_speed_factor
+
+        # Update rect object from self.center.
+        self.rect.y = self.y
+
+    def blitme(self):
+        """Draw the rocket at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+
+def fire_bullet(settings, screen, rocket, bullets):
+    """Fire a bullet if limit not reached yet."""
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < settings.bullets_limit:
+        new_bullet = Bullet(settings, screen, rocket)
+        bullets.add(new_bullet)
+
+
+def check_keydown_events(event, settings, screen, rocket, bullets):
+    """Respond to key presses."""
+    if event.key == pygame.K_UP:
+        rocket.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        rocket.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(settings, screen, rocket, bullets)
+
+
+def check_keyup_events(event, rocket):
+    """Respond to key releases."""
+    if event.key == pygame.K_UP:
+        rocket.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        rocket.moving_down = False
+
+
+def check_events(settings, screen, rocket, bullets):
+    """Respond to key presses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, settings, screen, rocket, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, rocket)
+
+
+def update_screen(settings, screen, rocket, bullets):
+    """Update images on the screen and flip to the new screen."""
+    # Redraw the screen during each pass through the loop.
+    screen.fill(settings.bg_color)
+    rocket.blitme()
+
+    # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
+
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet position.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.left > bullet.screen_rect.right:
+            bullets.remove(bullet)
+
+
+def run():
+    """Initialize pygame, settings and screen objects."""
+    pygame.init()
+    settings = Settings()
+    screen = pygame.display.set_mode(
+        (settings.screen_width, settings.screen_height))
+    pygame.display.set_caption('Rocket')
+
+    rocket = Rocket(settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
+
+    # Main Loop
+    while True:
+        check_events(settings, screen, rocket, bullets)
+        rocket.update()
+        update_bullets(bullets)
+        update_screen(settings, screen, rocket, bullets)
+
+
+run()
