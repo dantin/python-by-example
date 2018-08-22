223f50c81c7c3e660cf12120764f72c556b7f20b
David Ding <chengjie.ding@gmail.com>
2018-08-22 17:07:00 +0800

    chapter14 of python crash course. (#18)
    * support 'play' button.
    
    * support game speedup.
    
    * add scoreboard.
    
    * support update score.
    
    * support updated high score.
    
    * display level on the screen.
    
    * display the number of ships.
    
    * exec.

--- crash_course/alien_invasion/ship.py
+++ crash_course/alien_invasion/ship.py
@@ -1,12 +1,15 @@
 
 import pygame
 
+from pygame.sprite import Sprite
 
-class Ship():
+
+class Ship(Sprite):
     """Ship class."""
 
     def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
