555e5c9b9b854d6adb1ff38febf27d0dc59a3ebf
dantin <chengjie.ding@gmail.com>
2018-08-23 10:33:46 +0800

    plotting the start and end points.

--- crash_course/ch15/scatter_squares4.py
+++ crash_course/ch15/scatter_squares4.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 # plt.scatter(x_values, y_values, s=40)
-# plt.scatter(x_values, y_values, edgecolor='none', s=40)
-# plt.scatter(x_values, y_values, c='red', edgecolor='none', s=40)
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+# plt.scatter(x_values, y_values, edgecolors='none', s=40)
+# plt.scatter(x_values, y_values, c='red', edgecolors='none', s=40)
+plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolors='none', s=40)
 
 # Set chart title and label size.
 plt.title('Square Numbers', fontsize=24)
