00dd90eed68c71a9c1409e87026a6c5414414f13
dantin <chengjie.ding@gmail.com>
2018-08-23 13:07:37 +0800

    exec.

---
+++ crash_course/ch15/exec/three_dice.py
@@ -0,0 +1,49 @@
+
+import pygal
+
+from die import Die
+
+
+def get_max_result(dices):
+    """Get max result of dices list."""
+    max_result = 0
+    for die in dices:
+        max_result += die.num_sides
+    return max_result
+
+
+def get_roll_result(dices):
+    """Get roll results."""
+    result = 0
+    for die in dices:
+        result += die.roll()
+    return result
+
+
+# Create three D6s.
+dices = [Die(), Die(), Die()]
+
+max_result = get_max_result(dices)
+
+# Make some rolls, and store results in a list.
+results = []
+for roll_num in range(1000):
+    result = get_roll_result(dices)
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+for value in range(len(dices), max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+hist = pygal.Bar()
+
+hist.title = 'Results of rolling three D6 1000 times.'
+hist.x_labels = [str(x) for x in range(len(dices), max_result+1)]
+hist.x_title = 'Result'
+hist.y_title = 'Frequency of Result'
+
+hist.add('Three D6', frequencies)
+hist.render_to_file('die_visual.svg')
