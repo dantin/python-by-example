3e10bf16eaacbe2776c393ede76580222a08acbf
dantin <chengjie.ding@gmail.com>
2018-08-24 16:12:46 +0800

    demo that list python projects on github.

--- crash_course/ch17/python_repos.py
+++ crash_course/ch17/python_repos.py
@@ -9,6 +9,16 @@ print('Status code:', r.status_code)
 
 # Store API response in a variable.
 response_dict = r.json()
+print('Total repositories:', response_dict['total_count'])
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print('Repositories returned:', len(repo_dicts))
+
+print('\nSelected information about each repository:')
+for repo_dict in repo_dicts:
+    print('\nName:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
