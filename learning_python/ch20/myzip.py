92580f45dcc17f02f3d3dbac1844d8cf62557f48
David Ding <chengjie.ding@gmail.com>
2019-03-04 11:18:18 +0800

    update. (#22)

---
+++ learning_python/ch20/myzip.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+def myzip(*seqs):
+    seqs = [list(S) for S in seqs]
+    res = []
+    while all(seqs):
+        res.append(tuple(S.pop(0) for S in seqs))
+    return res
---
+++ learning_python/ch21/timeseqs_timer2.py
@@ -0,0 +1,44 @@
+#-*- coding: utf-8 -*-
+"""Test the relative speed of iteration tool alternatives"""
+
+import sys
+import timer2
+
+
+reps = 10000
+repslist = list(range(reps))
+
+
+def forLoop():
+    res = []
+    for x in repslist:
+        res.append(x + 10)
+    return res
+
+
+def listComp():
+    return [x + 10 for x in repslist]
+
+
+def mapCall():
+    return list(map((lambda x: x + 10), repslist))
+
+
+def genExp():
+    return list(x + 10 for x in repslist)
+
+
+def genFunc():
+    def gen():
+        for x in repslist:
+            yield x + 10
+    return list(gen())
+
+
+
+if __name__ == '__main__':
+    print(sys.version)
+    for test in (forLoop, listComp, mapCall, genExp, genFunc):
+        (total, result) = timer2.bestoftotal(test, _reps1=5, _reps=1000)
+        print('%-9s: %.5f ==> [%s..%s]' % 
+                (test.__name__, total, result[0], result[-1]))
